d3723f895215e07b68f1844d2fedaa585aa909b2
winnieBear <fengyang321@gmail.com>
2016-04-08 16:54:31 +0800

    fis-conf.js修改

--- fis-conf.js
+++ fis-conf.js
@@ -1,9 +1,10 @@
 //发布路径设置
 fis.set('release', {
-    'dir': 'output',
-    /*'watch': true,
-    'live': true,*/
-    /*'lint': true,*/
+    //构建结果输出的路径
+    /*'dir': 'output',*/
+    'watch': true,
+    'live': true,
+    'lint': true,
     'clean': true,
     //每次release的时候是否把release目录清空，
     //注意，如果启动watch/live时，需要把clean设置为true，因为默认只是增量release，而每次清空目录，每次只会重新构建变动的文件
@@ -15,6 +16,8 @@ fis.set('project.files', [
 ]);
 fis.set('release.jsPkgDir','js');
 
+//---------------需要自己修改的配置 START -----------------
+//根据你自己打包的文件进行配置
 var pageFiles = ['index.html', 'page1.html'];
 var page_config = {
     'src/index.html': 'index',
@@ -22,12 +25,18 @@ var page_config = {
 };
 
 var urlPre = '/your/project/prepath/xxx'; // 项目路径前缀
+var jsPkgBaseUrl = 'http://j2.58cdn.com.cn/xxx/js/';
 
-//package 设置
+
+//---------------需要自己修改的配置 END -----------------
+
+//合并雪碧图的配置
 fis.match('::package', {
     spriter: fis.plugin('csssprites-plus', {
+        //合并雪碧图时每一个小图片之间的空隙
         margin: 10,
         layout: 'matrix',
+        //合成后的雪碧图存放的路径
         to: '/img'
     })
 });
@@ -56,7 +65,7 @@ fis.match('{*.html,manifest.json}', {
 });
 
 //特殊路径下的资源处理
-fis.match('src/(test/**)', {
+fis.match('src/test/**', {
     useHash: false
 });
 
@@ -70,7 +79,7 @@ fis.match('/src/test/server.conf', {
     release: '/config/server.conf'
 });
 
-fis.match('src/js/(lib/**)', {
+fis.match('src/js/lib/**', {
     useHash: false
 });
 
@@ -126,15 +135,16 @@ fis
                 "no-undef": 2,
             },
             //envs:[],
-            globals: [ //这里配置你自己的全局变量
+            globals: [ 
+                //这里配置你自己的全局变量
                 'define',
                 'require'
             ]
         })
     });
 //------------------------------------代码校验END---------------------------
-// 使用符合AMD规范的requirejs
-// 配置详解：
+
+// AMD模块配置详解：
 // baseUrl: 默认为. 即项目根目录,用来配置模块查找根目录
 // paths: 用来设置别名,路径基于baseUrl设置
 // packages: 用来配置包信息,方便项目中引用
@@ -152,114 +162,112 @@ fis.hook('amd', {
     }*/
 });
 
-// 利用wn-pack进行模块依赖加载
+
+/*
+    利用fis3-packager-wn-pack进行css/amd模块的js/外链的js的构建，
+    dev阶段对css和js不进行合并，但是会在html中生成内联的require.config配置，
+    确保能够按需加载对应的模块文件
+ */
 // https://github.com/winnieBear/fis3-packager-wn-pack
 fis.match('::package', {
     packager: fis.plugin('wn-pack', {
         page: {
             files: pageFiles,
-            // 打包页面异步请求的js配置
+            //dev阶段异步请求的js不打包，按需加载
             packAsync: false,
-            //打包页面中外链的css的配置
+            //dev阶段外链的css不需要打包
             packCss:false,
-            //打包页面中外链的css的配置
+            //dev阶段外链的js不需要打包
             packJs:false,
         },
-        // 内联 `require.config`
+        // dev阶段生成内联 `require.config`
         inlineResourceConfig: true,
         //最终生成的require.config的配置项
         amdConfig: {
             baseUrl:'/js/'
         },
+        //dev阶段也生成pathmap，为了按需加载到正确的文件，因为每一个模块文件也进行hash处理了，直接加载模块的名字，文件是不存在的
         outputNotPackPathMap: function(file){
             return file.isJsLike && file.isMod;
         }
-    }),
-    /*postpackager: fis.plugin('loader', {
-        resourceType: 'amd',
-        useInlineMap: true,
-        allInOne: {
-            includeAsyncs: true,
-        }
-    })*/
+    })
+   
 });
 
 
 
 
 /**
- *  test/preqa/qa/prod media在
- *  package 阶段的配置对象
+ *  test/preqa/qa/prod media在 package 阶段的配置对象
+ *  
+ * 利用fis3-packager-wn-pack进行css/amd模块的js/外链的js的构建，
+ * 每一个页面会生成一个boot文件，文件是该页面的require.config配置
+ * 同时每一个页面会生成一个或多个pkg文件
  */
-var proPackageOptions = {
-    packager: fis.plugin('wn-pack', {
-        page: {
-            files: pageFiles,
-            // 打包页面异步请求的js配置
-            packAsync: true,
-            //打包页面中外链的css的配置
-            packCss: {
-                target:function(defaultPackFile, page){
-                    var configFile = page_config[page.id];
-                    if (!configFile) {
-                        fis.log.error('page_config[' + page.id + '] is not set!');
-                        return defaultConfigFile;
-                    }
-                    return '/css/pkg_'+configFile+'.css'
-                }
-            },
-            //打包页面中外链的js的配置
-            packJs:{
-                target:function(defaultPackFile, page){
-                    var configFile = page_config[page.id];
-                    if (!configFile) {
-                        fis.log.error('page_config[' + page.id + '] is not set!');
-                        return defaultConfigFile;
-                    }
-                    return '/js/pkg/pkg_'+configFile+'.js'
-                }
-            }
-        },
-        bundles:[
-            {
-                files: ['/src/js/mod/base/base.js'],
-                target: '/js/pkg/common.js',
-                load:true
-            },{
-                files: ['/src/scss/reset.scss'],
-                target: '/css/pkg_common.css',
-                load:true
-            }
-        ],
-        // 内联 `require.config`
-        inlineResourceConfig: false,
-        //最终生成的require.config的配置项
-        amdConfig: {
-            //baseUrl:'http://j2.58cdn.com.cn/js/'
-            baseUrl:'/js/'
-        },
-        //最终生成的require.config文件的名字
-        resourceConfigFile: function(defaultConfigFile, page) {
-            var configFile = page_config[page.id];
-            if (!configFile) {
-                fis.log.error('page_config[' + page.id + '] is not set!');
-                return defaultConfigFile;
-            }
-            return '/js/conf/boot_' + configFile + '.js';
-        },
-        //没有打包的资源是否生成pathmap，在dev环境下需要生成，以方便按需加载调试
-        outputNotPackPathMap: false,
-        //是否需要把异步请求合成后的的js文件输出到页面上，缺省是false
-        outputAsynPkg:false
-    }),
-    /*postpackager: fis.plugin('loader', {
-        resourceType: 'amd',
-        useInlineMap: true,
-        allInOne: {
-            includeAsyncs: true,
+
+var fis3_packager_wn_pack_options = {
+    //多个页面公共的css和js打包配置，可以配置多个合成规则
+    //注意异步的公共模块合成到异步的公共pkg里，同步的公共js合成到同步的公共pkg里面，不要混到一块
+    bundles:[
+        {
+            files: ['/src/js/mod/base/base.js'],
+            target: '/js/pkg/common.js',
+            load:true
+        },{
+            files: ['/src/scss/reset.scss'],
+            target: '/css/pkg_common.css',
+            load:true
         }
-    })*/
-}; // end proPackageOptions
+    ],
+    page: {
+        files: pageFiles,
+        //异步请求的js(排除bundles已经合成过的文件)进行合并
+        packAsync: true,
+        //外链的css(排除bundles已经合成过的文件)合成为一个css文件，合成后的文件名字通过packCss.target函数生成
+        packCss: {
+            target:function(defaultPackFile, page){
+                var configFile = page_config[page.id];
+                if (!configFile) {
+                    fis.log.error('page_config[' + page.id + '] is not set!');
+                    return defaultConfigFile;
+                }
+                return '/css/pkg_'+configFile+'.css'
+            }
+        },
+        //外链的js(排除bundles已经合成过的文件)合成为一个js文件，合成后的文件名字通过packJs.target函数生成
+        packJs:{
+            target:function(defaultPackFile, page){
+                var configFile = page_config[page.id];
+                if (!configFile) {
+                    fis.log.error('page_config[' + page.id + '] is not set!');
+                    return defaultConfigFile;
+                }
+                return '/js/pkg/pkg_'+configFile+'.js'
+            }
+        }
+    },
+    //生成的boot文件外链
+    inlineResourceConfig: false,
+    //最终生成的require.config的配置项
+    amdConfig: {
+        //baseUrl:'http://j2.58cdn.com.cn/js/'
+        baseUrl:'/js/'
+    },
+    //最终生成的config文件的名字可以通过resourceConfigFile函数修改
+    resourceConfigFile: function(defaultConfigFile, page) {
+        var configFile = page_config[page.id];
+        if (!configFile) {
+            fis.log.error('page_config[' + page.id + '] is not set!');
+            return defaultConfigFile;
+        }
+        return '/js/conf/boot_' + configFile + '.js';
+    },
+    //在非dev环节，没有被打包的资源在boot文件中不生成pathmap
+    outputNotPackPathMap: false,
+    //异步请求js文件打包后不通过外链的方式输出到页面上，而是让它异步自动加载
+    outputAsynPkg:false
+};
+// end proPackageOptions
 
 
 
@@ -272,7 +280,9 @@ fis.media('test')
     .match('*.{css,scss}', {
         useSprite: true,
     })
-    .match('::package', proPackageOptions);
+    .match('::package', {
+        packager: fis.plugin('wn-pack', fis3_packager_wn_pack_options)
+    });
 
 
 /**
@@ -284,7 +294,9 @@ fis.media('pre-qa')
     .match('*.{css,scss}', {
         useSprite: true,
     })
-    .match('::package', proPackageOptions)
+    .match('::package', {
+        packager: fis.plugin('wn-pack', fis3_packager_wn_pack_options)
+    })
     .match('*', {
         deploy: [
             // 过滤掉已经被打包的资源.
@@ -308,6 +320,12 @@ fis.media('pre-qa')
  * 所有资源发布到publish路径
  * 所有资源不压缩
  */
+var _ = fis.util;
+var fis3_packager_wn_pack_options_qa = _.assign({},fis3_packager_wn_pack_options);
+fis3_packager_wn_pack_options_qa['amdConfig']['baseUrl'] = jsPkgBaseUrl;
+
+console.info(fis3_packager_wn_pack_options_qa);
+
 fis.media('qa')
     .match('{test/*,config/*}', {
         release: false
@@ -326,7 +344,9 @@ fis.media('qa')
         useHash:false,
         domain: 'http://j1.58cdn.com.cn' + urlPre
     })
-    .match('::package', proPackageOptions)
+    .match('::package', {
+        packager: fis.plugin('wn-pack', fis3_packager_wn_pack_options_qa)
+    })
     .match('*', {
         deploy: [
             fis.plugin('skip-packed', {
@@ -373,7 +393,9 @@ fis.media('prod')
         useHash:false,
         domain: 'http://j1.58cdn.com.cn' + urlPre
     })
-    .match('::package', proPackageOptions)
+    .match('::package', {
+        packager: fis.plugin('wn-pack', fis3_packager_wn_pack_options_qa)
+    })
     .match('*', {
         deploy: [
             fis.plugin('skip-packed', {
@@ -420,7 +442,9 @@ fis.media('deploy-ftp')
         useHash:false,
         domain: 'http://j1.58cdn.com.cn' + urlPre
     })
-    .match('::package', proPackageOptions)
+    .match('::package', {
+        packager: fis.plugin('wn-pack', fis3_packager_wn_pack_options_qa)
+    })
     /* .match('*.html', {
          //需要自己安装fis-optimizer-html-minifier插件
          optimizer: fis.plugin('html-minifier')
